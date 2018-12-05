3c0ab6438c3210a77805509d259b826ffd461526
wuxiangang <wuxiangang@bitboolean.com>
2018-12-05 09:42:57 +0800

    first

---
+++ px2rem.js
@@ -0,0 +1,49 @@
+var fs = require('fs')
+var path=require('path');
+var config = require('./config/config.json');
+var filePath = config.path;
+var htmlpx = 20;
+var types = config.filterTypes;
+
+function exists(path){  
+     return fs.existsSync(path)
+} 
+
+function callback (v, a, c) {
+	return parseInt(v) / htmlpx + 'rem'
+}
+
+function transform (file, fontPath, e) {
+	const currentPath = fontPath +'/'+ file;
+	if (fs.statSync(currentPath).isDirectory()) {
+		fs.readdir(currentPath, 'utf8', function (err,data) {
+			data.forEach((item, index) => {
+			  	return transform(item, currentPath)
+			})
+		})
+	} else {
+		if (!types.includes(file.match(/\.((?!\.).)*$/)[0].replace(/\./, ''))) return
+		if (config.minJs === 'false' && file.includes('.min')) return
+			console.log(currentPath)
+		fs.readFile(currentPath,'utf8',function(err,files){
+			var result = files.replace(/\d+px(?!(\s*\)))/g, callback);
+			fs.writeFile(currentPath, result, 'utf8', function (err) {
+			     if (err) return console.log(err);
+			});
+ 
+		})
+	}
+}	
+
+//readdir方法读取文件名
+//readFile方法读取文件内容
+//writeFile改写文件内容
+filePath.forEach((dir, i) => {
+	if (!exists(dir)) return console.log('not exist', dir);
+	fs.readdir(dir, 'utf8', function (err,data) {
+		data.forEach((item, index) => {
+			transform(item, dir)
+		});
+	 
+	})
+})
